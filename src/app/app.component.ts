fe53deb17452011ed4f6fd2d456cd6bece00e054
vargavd <vvd007@gmail.com>
2022-12-13 19:26:06 +0100

    Hover effects directive finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,47 @@ import { Texts } from 'src/sample-data';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  texts: string[] = Texts;
+  cardInfos: { 
+    mouseIsOverDelete: boolean,
+    mouseIsOverAdd: boolean,
+    text: string
+  } [] = Texts.map(text => ({
+    mouseIsOverDelete: false,
+    mouseIsOverAdd: false,
+    text
+  }));
+
+  // mouse events
+  mouseenterDeleteButton(cardIndex: number) {
+    if (cardIndex < 0 || cardIndex > this.cardInfos.length) {
+      console.error(`cardIndex is out of boundary: ${cardIndex}`);
+      return;
+    }
+
+    this.cardInfos[cardIndex].mouseIsOverDelete = true;
+  }
+  mouseleaveDeleteButton(cardIndex: number) {
+    if (cardIndex < 0 || cardIndex > this.cardInfos.length) {
+      console.error(`cardIndex is out of boundary: ${cardIndex}`);
+      return;
+    }
+
+    this.cardInfos[cardIndex].mouseIsOverDelete = false;
+  }
+  mouseenterAddButton(cardIndex: number) {
+    if (cardIndex < 0 || cardIndex > this.cardInfos.length) {
+      console.error(`cardIndex is out of boundary: ${cardIndex}`);
+      return;
+    }
+
+    this.cardInfos[cardIndex].mouseIsOverAdd = true;
+  }
+  mouseleaveAddButton(cardIndex: number) {
+    if (cardIndex < 0 || cardIndex > this.cardInfos.length) {
+      console.error(`cardIndex is out of boundary: ${cardIndex}`);
+      return;
+    }
+
+    this.cardInfos[cardIndex].mouseIsOverAdd = false;
+  }
 }
--- src/app/card-hover-effets.directive.ts
+++ src/app/card-hover-effets.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostBinding, HostListener, DoCheck } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appCardHoverEffets]'
@@ -6,20 +6,93 @@ import { Directive, ElementRef, HostBinding, HostListener, DoCheck } from '@angu
 export class CardHoverEffetsDirective {
   // element styles
   @HostBinding('style.boxShadow') boxShadow: string;
+  @HostBinding('style.opacity') opacity: string;
+  @HostBinding('style.transform') transform: string;
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
-  // define styles
+  // states
+  _mouseOverCard: boolean = false;
+  _mouseOverDeleteButton: boolean = false;
+  _mouseOverAddButton: boolean = false;
+
+  // Input properties
+  @Input() set mouseOverDeleteButton(value: boolean) {
+    this._mouseOverDeleteButton = value;
+
+    this.setStyles();
+  };
+  @Input() set mouseOverAddButton(value: boolean) {
+    this._mouseOverAddButton = value;
+    
+    this.setStyles();
+  };
+
+  // default styles
   defaultBoxShadow = '0 0 0 rgba(0, 0, 0, 0.2)';
+  defaultOpacity = '1';
+  defaultTransform = 'scale(1)';
+  defaultBackgroundColor = 'white';
+  
+  // highlighted styles
   highlightedBoxShadow = '0 0 10px rgba(0, 0, 0, 0.2)';
+
+  // over delete styles
+  deleteOverOpacity = '0.6';
+  deleteOverTransform = 'scale(0.96) rotateZ(0.5deg)';
+  deleteOverBackgroundColor = 'rgba(255, 240, 240, 1)';
+
+  // over add styles
+  addOverTransform = 'scale(1.06) rotateZ(-0.5deg)';
+  addOverBackgroundColor = 'rgba(240, 240, 255, 1)';
 
   // DOM events
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.boxShadow = this.highlightedBoxShadow;
+    this._mouseOverCard = true;
+
+    this.setStyles();
   }
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.boxShadow = this.defaultBoxShadow;
+    this._mouseOverCard = false;
+    
+    this.setStyles();
+  }
+
+  setStyles() {
+    // declare the styles which we will set
+    let boxShadowToSet = this.defaultBoxShadow;
+    let opacityToSet = this.defaultOpacity;
+    let transformToSet = this.defaultTransform;
+    let backgroundColorToSet = this.defaultBackgroundColor;
+
+    // state: mouse over on card
+    if (this._mouseOverCard) {
+      boxShadowToSet = this.highlightedBoxShadow;
+    }
+
+    // state: mouse is over the delete button
+    if (this._mouseOverDeleteButton) {
+      opacityToSet = this.deleteOverOpacity;
+      transformToSet = this.deleteOverTransform;
+      backgroundColorToSet =  this.deleteOverBackgroundColor;
+    }
+
+    // state: mouse is over the add button
+    if (this._mouseOverAddButton) {
+      transformToSet = this.addOverTransform;
+      backgroundColorToSet = this.addOverBackgroundColor;
+    }
+
+    // set the styles
+    this.boxShadow = boxShadowToSet;
+    this.opacity = opacityToSet;
+    this.transform = transformToSet;
+    this.backgroundColor = backgroundColorToSet;
   }
 
   constructor(private elementRef: ElementRef) {
     this.boxShadow = this.defaultBoxShadow;
+    this.opacity = this.defaultOpacity;
+    this.transform = this.defaultTransform;
+    this.backgroundColor = this.defaultBackgroundColor;
   }
 }
